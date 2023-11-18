e5cebb06cdb29d2082d2dfb498ed8c5d79450bde
MostafaAlii <mostafa0alii@gmail.com>
2023-11-18 17:54:35 +0200

    start with free order

--- resources/views/website/layouts/dashboard.blade.php
+++ resources/views/website/layouts/dashboard.blade.php
@@ -1,5 +1,7 @@
 @include('website.layouts.common.client._tpl_start')
 @include('website.dashboard.includes._navbar')
+{{--@include('website.layouts.common.web._header')--}}
 @yield('content')
 @include('website.layouts.common.client._footer')
+{{--@include('website.layouts.common.web._footer')--}}
 @include('website.layouts.common.client._tpl_end')
